c51e5df5c311459c188c13fb87c0bf9a9ad348a4
Simon Otter <simon@otter.se>
2014-02-26 00:31:52 +0100

    Added a fugly dialog to threshold. Inverter works for colours.

--- imagej/SO_Threshold.java
+++ imagej/SO_Threshold.java
@@ -19,11 +19,20 @@ public class SO_Threshold implements PlugInFilter {
 		int w = ip.getWidth();
 		int h = ip.getHeight();
 
+		GenericDialog gd = new GenericDialog("Threshold");
+		gd.addSlider("Value: ", 0.0, 255.0, (double) THRESHOLD);
+
+		gd.showDialog();
+		if (gd.wasCanceled()) 
+			return;
+
+		int th = (int) gd.getNextNumber();
+
 		for (int v = 0; v < h; v++)
 		{
 			for (int u = 0; u < w; u++)
 			{
-				int p = ip.get(u, v) < THRESHOLD? 0 : 255;
+				int p = ip.get(u, v) < th? 0 : 255;
 				ip.set(u, v, p);
 			}
 		}
--- imagej/Simon_Inverter.java
+++ imagej/Simon_Inverter.java
@@ -9,13 +9,23 @@ public class Simon_Inverter implements PlugInFilter {
 
 	public int setup(String arg, ImagePlus imp) {
 		this.imp = imp;
-		return DOES_8G;
+		return DOES_8G | DOES_RGB;
 	}
 
 	public void run(ImageProcessor ip) {
 		int w = ip.getWidth();
 		int h = ip.getHeight();
 
+		if (ip.getNChannels() == 1) {
+			invertGrayscale(w, h, ip);
+		} else if (ip.getNChannels() == 3) {
+			invertRGB8bpp(w, h, ip);
+		} else {
+			throw new RuntimeException("Only supports 8-bit Grayscale or 8bpp RGB.");
+		}
+	}
+
+	private void invertGrayscale(int w, int h, ImageProcessor ip) {
 		for (int v = 0; v < h; v++)
 		{
 			for (int u = 0; u < w; u++)
@@ -27,4 +37,19 @@ public class Simon_Inverter implements PlugInFilter {
 		}
 	}
 
+	private void invertRGB8bpp(int w, int h, ImageProcessor ip) {
+		for (int v = 0; v < h; v++)
+		{
+			for (int u = 0; u < w; u++)
+			{
+				int p = ip.get(u, v);
+
+				int r = 255 - ((p & 0xFF0000) >> 16);
+				int g = 255 - ((p & 0x00FF00) >> 8);
+				int b = 255 - ((p & 0x0000FF) >> 0);
+
+				ip.set(u, v, (r << 16) | (g << 8) | b);
+			}
+		}
+	}
 }
